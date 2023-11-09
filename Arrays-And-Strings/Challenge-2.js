10df518fb922e804c0bc9e60ab2eca3f23cd47f8
Chao Zhou <Czhou0k@gmail.com>
2023-11-09 12:21:14 -0500

    added my own solution to challenge two

--- Arrays-And-Strings/Challenge-2.js
+++ Arrays-And-Strings/Challenge-2.js
@@ -1,1 +1,9 @@
-const permutationChecker = (string1, string2) => {};
+const permutationChecker = (string1, string2) => {
+  if (string1.length !== string2.length) {
+    return false;
+  }
+  const sortedStringOne = string1.split('').sort().join('');
+  const sortedStringTwo = string2.split('').sort().join('');
+
+  return sortedStringOne === sortedStringTwo ? true : false;
+};
