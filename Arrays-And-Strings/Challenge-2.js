509fdab94e6520d5fde7758c2e6dc90c642d7bee
Chao Zhou <Czhou0k@gmail.com>
2023-11-10 11:25:29 -0500

    added another solution with hashmap

--- Arrays-And-Strings/Challenge-2.js
+++ Arrays-And-Strings/Challenge-2.js
@@ -8,6 +8,34 @@ const permutationChecker = (string1, string2) => {
   return sortedStringOne === sortedStringTwo ? true : false;
 };
 
-console.log(permutationChecker('abd', 'dba')); //true
-console.log(permutationChecker('abcsdaease', 'ffe')); //false
-console.log(permutationChecker('', '')); // true
+// console.log(permutationChecker('abd', 'dba')); //true
+// console.log(permutationChecker('abcsdaease', 'ffe')); //false
+// console.log(permutationChecker('', '')); // true
+
+//Using a hashmap
+
+const permutationChecker2 = (string1, string2) => {
+  if (string1.length !== string2.length) {
+    return false;
+  }
+  const hashMap = {};
+  for (let i = 0; i < string2.length; i++) {
+    const character = string2[i];
+    if (hashMap[character]) {
+      hashMap[character] += 1;
+    } else {
+      hashMap[character] = 1;
+    }
+  }
+  for (let i = 0; i < string1.length; i++) {
+    const character = string1[i];
+    if (!hashMap[character]) {
+      return false;
+    } else if (hashMap[character] > 0) {
+      hashMap[character] -= 1;
+    } else {
+      return false;
+    }
+  }
+  return true;
+};
