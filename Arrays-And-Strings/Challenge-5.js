4aca8b1409e1fa1bf644430bd21a317e58ca7d74
Chao Zhou <Czhou0k@gmail.com>
2023-11-13 12:35:01 -0500

    added personal solution to challenge

---
+++ Arrays-And-Strings/Challenge-5.js
@@ -0,0 +1,37 @@
+const oneAway = (string1, string2) => {
+  if (Math.abs(string1.length - string2.length) > 1) {
+    return false;
+  }
+  const hashMap = {};
+  let extraCharacterCount = 0;
+
+  for (let i = 0; i < string1.length; i++) {
+    const character = string1[i];
+
+    if (hashMap[character]) {
+      hashMap[character] += 1;
+    } else {
+      hashMap[character] = 1;
+    }
+  }
+
+  for (let i = 0; i < string2.length; i++) {
+    const character = string2[i];
+
+    if (hashMap[character]) {
+      hashMap[character] -= 1;
+    } else {
+      extraCharacterCount += 1;
+    }
+  }
+  let count = 0;
+  const characterValues = Object.values(hashMap);
+
+  for (let i = 0; i < characterValues.length; i++) {
+    const value = characterValues[i];
+
+    count += value;
+  }
+  console.log(extraCharacterCount);
+  return count + extraCharacterCount <= 2;
+};
