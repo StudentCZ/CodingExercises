53dc97ce8c88501c69c6b2be72c6c84d4c85c339
Chao Zhou <Czhou0k@gmail.com>
2023-11-15 10:06:31 -0500

    added a solution to challenge 6

---
+++ Arrays-And-Strings/Challenge-6.js
@@ -0,0 +1,19 @@
+const compressed = (string) => {
+  let pointerOne = 0;
+  let pointerTwo = 1;
+  const result = [];
+
+  while (pointerOne < string.length) {
+    if (string[pointerOne] === string[pointerTwo]) {
+      pointerTwo += 1;
+    } else {
+      let repeat = pointerTwo - pointerOne;
+      result.push(`${string[pointerOne]}${repeat}`);
+      pointerOne = pointerTwo;
+      pointerTwo += 1;
+    }
+  }
+  return result.join('').length < string.length ? result.join('') : string;
+};
+
+console.log(compressed('aaaaabcccl'));
