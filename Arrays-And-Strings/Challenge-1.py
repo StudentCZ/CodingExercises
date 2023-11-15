793fa8fd331b24ffd8e24b044bd5c337f25b43c1
Chao Zhou <Czhou0k@gmail.com>
2023-11-15 10:36:57 -0500

    added alternative python solution to challenge 1

--- Arrays-And-Strings/Challenge-1.py
+++ Arrays-And-Strings/Challenge-1.py
@@ -7,5 +7,16 @@ def unique_string(string):
       dictionary[character] = True
   return True
 
-print(unique_string('')) # True
-print(unique_string('aab')) # False
+# print(unique_string('')) # True
+# print(unique_string('aab')) # False
+
+def unique_string2(string):
+  sorted_string = ''.join(sorted(string))
+  for i in range(1, len(sorted_string)):
+    character = sorted_string[i]
+    previous_character = sorted_string[i-1]
+    if (character == previous_character):
+      return False
+  return True
+
+print(unique_string2('happy'))
