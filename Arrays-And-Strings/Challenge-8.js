fe611193094696115f4fe74a35813b90ea15c1c7
Chao Zhou <Czhou0k@gmail.com>
2023-11-19 13:25:58 -0500

    added a working solution to Challenge 8

---
+++ Arrays-And-Strings/Challenge-8.js
@@ -0,0 +1,33 @@
+const zeroMatrix = (matrix) => {
+  const rows = matrix.length;
+  const columns = matrix[0].length;
+
+  const zeroRows = Array(rows).fill(false);
+  const zeroColumns = Array(columns).fill(false);
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < columns; j++) {
+      if (matrix[i][j] === 0) {
+        zeroRows[i] = true;
+        zeroColumns[j] = true;
+      }
+    }
+  }
+
+  for (let i = 0; i < rows; i++) {
+    if (zeroRows[i] === true) {
+      for (let j = 0; j < columns; j++) {
+        matrix[i][j] = 0;
+      }
+    }
+  }
+
+  for (let j = 0; j < columns; j++) {
+    if (zeroColumns[j] === true) {
+      for (let i = 0; i < rows; i++) {
+        matrix[i][j] = 0;
+      }
+    }
+  }
+  return matrix;
+};
