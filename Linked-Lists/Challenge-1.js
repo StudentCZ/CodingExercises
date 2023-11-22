a31a90f3e883aae4da9177ad74807fd408e2f146
Chao Zhou <Czhou0k@gmail.com>
2023-11-22 14:43:55 -0500

    added the setup template for the problem

--- Linked-Lists/Challenge-1.js
+++ Linked-Lists/Challenge-1.js
@@ -4,3 +4,16 @@ class Node {
     this.next = null;
   }
 }
+
+const node1 = new Node('A');
+const node2 = new Node('B');
+const node3 = new Node('A');
+const node4 = new Node('C');
+
+node1.next = node2;
+node2.next = node3;
+node3.next = node4;
+
+const removeDuplicateNodes = (head) => {
+  let current = head;
+};
