009d12b342fd75324f3c100fff26405a494f3590
Chao Zhou <Czhou0k@gmail.com>
2023-11-24 15:54:08 -0500

    revise solution to tackle edge cases

--- Arrays-And-Strings/Challenge-3.py
+++ Arrays-And-Strings/Challenge-3.py
@@ -3,10 +3,16 @@ def replace_string(string,true_length):
 
   for i in range(true_length):
     character = string[i]
-    if character == " ":
+    if character == " " and string[i+1] is not " ":
       new_string.append('$7')
+    elif character == " " and string[i+1] == " ":
+      continue
     else:
       new_string.append(character)
   return "".join(new_string)
 
+print(replace_string('', 0))
+print(replace_string('psd0', 4))
+print(replace_string('a b c ', 5))
+print(replace_string('a  b d  c f', 9))
 
