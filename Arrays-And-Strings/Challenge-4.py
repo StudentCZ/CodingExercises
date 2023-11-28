e8f3e8885b5f8a9001d7c913040f25129a196786
Chao Zhou <Czhou0k@gmail.com>
2023-11-28 15:22:41 -0500

    added python solution to challenge 4

---
+++ Arrays-And-Strings/Challenge-4.py
@@ -0,0 +1,19 @@
+def permutation_palindrome(string):
+  hash_map = {}
+
+  for character in string:
+    if character == ' ':
+      continue
+    if character in hash_map:
+      hash_map[character] += 1
+    else:
+      hash_map[character] = 1
+
+  character_count = list(hash_map.values())
+  odd_count = 0
+
+  for value in character_count:
+    if value % 2 == 1:
+      odd_count += 1
+  return odd_count <= 1
+
