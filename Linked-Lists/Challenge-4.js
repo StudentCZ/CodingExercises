974ff80711d81ee4b755830ba12cb842c8f2e7c5
Chao Zhou <Czhou0k@gmail.com>
2023-12-01 14:30:34 -0500

    added a solution to challenge 4 js file

--- Linked-Lists/Challenge-4.js
+++ Linked-Lists/Challenge-4.js
@@ -18,6 +18,7 @@ const createLinkedList = (values) => {
     return null;
   }
   let head = new Node(values[0]);
+  let current = head;
   for (let i = 1; i < values.length; i++) {
     let newNode = new Node(values[i]);
     current.next = newNode;
@@ -25,3 +26,42 @@ const createLinkedList = (values) => {
   }
   return head;
 };
+
+const values = [10, 1, 3, 6, 5, 2, 8];
+const head = createLinkedList(values);
+
+const partitionNode = (head, partition) => {
+  let current = head;
+  let leftPartition = null;
+  let rightPartition = null;
+  let leftHead = null;
+  let rightHead = null;
+
+  while (current !== null) {
+    if (current.val < partition) {
+      if (leftPartition === null) {
+        leftPartition = new Node(current.val);
+        leftHead = leftPartition;
+      } else {
+        let next = new Node(current.val);
+        leftPartition.next = next;
+        leftPartition = leftPartition.next;
+      }
+    } else {
+      if (rightPartition === null) {
+        rightPartition = new Node(current.val);
+        rightHead = rightPartition;
+      } else {
+        let next = new Node(current.val);
+        rightPartition.next = next;
+        rightPartition = rightPartition.next;
+      }
+    }
+    current = current.next;
+  }
+  leftPartition.next = rightHead;
+  return leftHead;
+};
+
+const leftHead = partitionNode(head, 5);
+processLinkedList(leftHead, 5);
