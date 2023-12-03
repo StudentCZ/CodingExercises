99bb6e419ec84ab01dfcd5fe7610d0c79ac87b75
Chao Zhou <Czhou0k@gmail.com>
2023-12-03 12:55:58 -0500

    added a linkedlist creator

--- Linked-Lists/Challenge-5.js
+++ Linked-Lists/Challenge-5.js
@@ -6,9 +6,21 @@ class Node {
 }
 
 const processLinkedList = (head) => {
-  const current = head;
+  let current = head;
   while (current !== null) {
     console.log(current.val);
     current = current.next;
   }
+};
+
+const createLinkedList = (values) => {
+  let head = new Node(values[0]);
+  let current = head;
+
+  for (let i = 1; i < values.length; i++) {
+    let newNode = new Node(values[i]);
+    current.next = newNode;
+    current = newNode;
+  }
+  return head;
 };
