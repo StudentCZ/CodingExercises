d6b01a87ba41827a0b953c54fdb4615a38800129
Chao Zhou <Czhou0k@gmail.com>
2023-12-06 12:14:01 -0500

    added solution to challenge 7

--- Linked-Lists/Challenge-7.js
+++ Linked-Lists/Challenge-7.js
@@ -13,3 +13,24 @@ const getLength = (head) => {
   }
   return length;
 };
+
+const findIntersection = (list1, list2) => {
+  let lengthOne = getLength(list1);
+  let lengthTwo = getLength(list2);
+
+  while (lengthOne > lengthTwo) {
+    list1 = list1.next;
+    lengthOne -= 1;
+  }
+
+  while (lengthTwo > lengthOne) {
+    list2 = list2.next;
+    lengthTwo -= 1;
+  }
+
+  while (list1 !== null && list2 !== null && list1 !== list2) {
+    list1 = list1.next;
+    list2 = list2.next;
+  }
+  return list1;
+};
