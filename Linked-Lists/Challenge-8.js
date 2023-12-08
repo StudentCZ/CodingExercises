b151c8d3ecf54b85ca45b7cfa33f0e03aed4f563
Chao Zhou <Czhou0k@gmail.com>
2023-12-08 13:33:52 -0500

    added a linked list creator to challenge 8

--- Linked-Lists/Challenge-8.js
+++ Linked-Lists/Challenge-8.js
@@ -4,3 +4,18 @@ class Node {
     this.next = null;
   }
 }
+
+const createLinkedList = (values) => {
+  let head = new Node(values[0]);
+  let current = head;
+
+  for (let i = 0; i < values.length; i++) {
+    let newNode = new Node(values[i]);
+    current.next = newNode;
+    current = newNode;
+  }
+  return head;
+};
+
+const values = ['A', 'B', 'C', 'D', 'E', 'C'];
+const head = createLinkedList(values);
