acfa1d780b92bdedd593f3d16e30a6bdd4a9cc76
Chao Zhou <Czhou0k@gmail.com>
2023-12-08 13:41:14 -0500

    added solution to challenge 8

--- Linked-Lists/Challenge-8.js
+++ Linked-Lists/Challenge-8.js
@@ -19,3 +19,27 @@ const createLinkedList = (values) => {
 
 const values = ['A', 'B', 'C', 'D', 'E', 'C'];
 const head = createLinkedList(values);
+
+const detectLoop = (head) => {
+  let slow = head;
+  let fast = head;
+
+  while (fast !== null && fast.next !== null) {
+    slow = slow.next;
+    fast = fast.next.next;
+
+    if (slow === fast) {
+      break;
+    }
+  }
+  if (fast === null || fast.next === null) {
+    return null;
+  }
+  fast = head;
+
+  while (slow !== fast) {
+    slow = slow.next;
+    fast = fast.next;
+  }
+  return slow;
+};
