035b696ce869761bf6ec3d1be7dbf8848ebed159
Chao Zhou <Czhou0k@gmail.com>
2023-12-11 14:56:10 -0500

    added python solution to Challenge 6

---
+++ Arrays-And-Strings/Challenge-6.py
@@ -0,0 +1,18 @@
+def compressed(string):
+    result = []
+    pointer_one = 0
+    pointer_two = 1
+
+    while pointer_one < len(string):
+        if pointer_two < len(string) and string[pointer_one] == string[pointer_two]:
+            pointer_two += 1
+        else:
+            count = pointer_two - pointer_one
+            result.append(f"{string[pointer_one]}{count}")
+            pointer_one = pointer_two
+            pointer_two += 1
+
+    compressed_string = "".join(result)
+    return string if len(string) <= len(compressed_string) else compressed_string
+
+print(compressed('aabcccccaaa'))
