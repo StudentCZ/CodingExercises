ee8184e6cf3bf5a59e658c26ba01fa591799bcd3
Chao Zhou <Czhou0k@gmail.com>
2023-12-12 18:54:35 -0500

    added python solution for challenge 7

---
+++ Arrays-And-Strings/Challenge-7.py
@@ -0,0 +1,18 @@
+def rotate_image(matrix):
+  matrix_length = len(matrix)
+
+  for i in range(matrix_length):
+    for j in range(i + 1, matrix_length):
+      matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+  for i in range(matrix_length):
+    matrix[i].reverse()
+  return matrix
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print(rotate_image(matrix))
