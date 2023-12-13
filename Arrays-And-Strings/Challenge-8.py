424dfff39d557046141534d51ab18118045b0760
Chao Zhou <Czhou0k@gmail.com>
2023-12-12 19:02:05 -0500

    added python solution to challenge 8

---
+++ Arrays-And-Strings/Challenge-8.py
@@ -0,0 +1,27 @@
+def zero_matrix(matrix):
+  rows, cols = len(matrix), len(matrix[0])
+  zero_rows, zero_cols = set(), set()
+
+  for i in range(rows):
+    for j in range(cols):
+      if matrix[i][j] == 0:
+        zero_rows.add(i)
+        zero_cols.add(j)
+
+  for row in zero_rows:
+    for j in range(cols):
+      matrix[row][j] = 0
+
+  for col in zero_cols:
+    for i in range(rows):
+      matrix[i][col] = 0
+
+  return matrix
+
+matrix = [
+    [1, 2, 3],
+    [4, 0, 6],
+    [7, 8, 9]
+]
+
+print(zero_matrix(matrix))
