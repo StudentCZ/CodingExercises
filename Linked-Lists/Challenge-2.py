f12f6b7e4665dfbfa52a358696a47ae620374a33
Chao Zhou <Czhou0k@gmail.com>
2023-12-14 12:02:47 -0500

    added a linked list creator to make it more reusable

--- Linked-Lists/Challenge-2.py
+++ Linked-Lists/Challenge-2.py
@@ -10,16 +10,19 @@ def print_linked_list(head):
     print(current.val)
     current = current.next
 
-head = Node(1)
-node2 = Node(2)
-node3 = Node(3)
-node4 = Node(4)
-node5 = Node(5)
+def create_linked_list(values):
+  head = Node(values[0])
+  current = head
 
-head.next = node2
-node2.next = node3
-node3.next = node4
-node4.next = node5
+  for value in values[1:]:
+    new_node = Node(value)
+    current.next = new_node
+    current = new_node
+
+  return head
+
+values = ['1','2','3','4','5','6']
+head = create_linked_list(values)
 
 
 def return_k_element(head,k):
@@ -36,5 +39,5 @@ def return_k_element(head,k):
   return pointer_two.val
 
 
-result = return_k_element(head, k)
+result = return_k_element(head, 2)
 print(result)
