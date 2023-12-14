d6c7853d4b5ef7a5fa5e52ff4833249a26daaff3
Chao Zhou <Czhou0k@gmail.com>
2023-12-14 11:52:21 -0500

    added a python solution to challenge 2

--- Linked-Lists/Challenge-2.py
+++ Linked-Lists/Challenge-2.py
@@ -9,3 +9,17 @@ def print_linked_list(head):
   while current is not None:
     print(current.val)
     current = current.next
+
+
+def return_k_element(head,k):
+  pointer_one = head
+  pointer_two = head
+
+  for i in range(0, k):
+    pointer_one = pointer_one.next
+
+  while pointer_one is not None:
+    pointer_one = pointer_one.next
+    pointer_two = pointer_two.next
+
+  return pointer_two.val
