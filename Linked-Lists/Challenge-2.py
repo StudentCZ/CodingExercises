340475576e426ce7f1745965d93c534949ddb72b
Chao Zhou <Czhou0k@gmail.com>
2023-12-14 11:55:28 -0500

    fixed error in loop and added a test

--- Linked-Lists/Challenge-2.py
+++ Linked-Lists/Challenge-2.py
@@ -10,12 +10,23 @@ def print_linked_list(head):
     print(current.val)
     current = current.next
 
+head = Node(1)
+node2 = Node(2)
+node3 = Node(3)
+node4 = Node(4)
+node5 = Node(5)
+
+head.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
+
 
 def return_k_element(head,k):
   pointer_one = head
   pointer_two = head
 
-  for i in range(0, k):
+  for i in range(k):
     pointer_one = pointer_one.next
 
   while pointer_one is not None:
@@ -23,3 +34,7 @@ def return_k_element(head,k):
     pointer_two = pointer_two.next
 
   return pointer_two.val
+
+
+result = return_k_element(head, k)
+print(result)
