d75739e126438299238c396efa29b689d2660d24
Chao Zhou <Czhou0k@gmail.com>
2023-12-16 18:41:52 -0500

    added linked list creator

--- Linked-Lists/Challenge-4.py
+++ Linked-Lists/Challenge-4.py
@@ -8,3 +8,15 @@ def print_linked_list(head):
   while current is not None:
     print(current.val)
     current = current.next
+
+def create_linked_list(values):
+  head = Node(values[0])
+  current = head
+  for value in values[1:]:
+    new_node = Node(value)
+    current.next = new_node
+    current = new_node
+  return head
+
+values = ['1','2','3','4','5','6','7','8']
+head = create_linked_list(values)
