f628189348230e5951befee18bff61adf6a702f2
Chao Zhou <Czhou0k@gmail.com>
2023-12-17 10:36:55 -0500

    added link list create function

--- Linked-Lists/Challenge-5.py
+++ Linked-Lists/Challenge-5.py
@@ -8,3 +8,13 @@ def print_linked_list(head):
   while current is not None:
     print(current.val)
     current = current.next
+
+def create_linked_list(values):
+  head = Node[values[0]]
+  current = head
+
+  for value in values[1:]:
+    new_node = Node[value]
+    current.next = new_node
+    current = new_node
+  return head
