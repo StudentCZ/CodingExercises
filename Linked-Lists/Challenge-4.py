2b11035ce19f3a22511c4976925d249fd8257e3b
Chao Zhou <Czhou0k@gmail.com>
2023-12-16 19:09:56 -0500

    created a working solution for challenge 4 in python

--- Linked-Lists/Challenge-4.py
+++ Linked-Lists/Challenge-4.py
@@ -18,5 +18,35 @@ def create_linked_list(values):
     current = new_node
   return head
 
-values = ['1','2','3','4','5','6','7','8']
+values = [1,8,10,4,5,6,3,2]
 head = create_linked_list(values)
+
+
+def partition_node(head, partition):
+  current = head
+  left_partition = None
+  right_partition = None
+  left_head = None
+  right_head = None
+
+  while current is not None:
+    if (current.val < partition):
+      if (left_partition is None):
+        left_partition = Node(current.val)
+        left_head = left_partition
+      else:
+        left_partition.next = Node(current.val)
+        left_partition = left_partition.next
+    else:
+      if (right_partition == None):
+        right_partition = Node(current.val)
+        right_head = right_partition
+      else:
+        right_partition.next = Node(current.val)
+        right_partition = right_partition.next
+    current = current.next
+  left_partition.next = right_head
+  return left_head
+
+new_node = partition_node(head,5)
+print_linked_list(new_node)
