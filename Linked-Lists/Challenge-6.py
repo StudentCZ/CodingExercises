dd6898bf1de7531e57eb2d9e8199f757e19f041e
Chao Zhou <Czhou0k@gmail.com>
2023-12-18 13:16:38 -0500

    added a linked list creator

--- Linked-Lists/Challenge-6.py
+++ Linked-Lists/Challenge-6.py
@@ -9,3 +9,13 @@ def print_linked_list(head):
     print(current.val)
     current = current.next
 
+def create_linked_list(values):
+  head = Node(values[0])
+  current = head
+
+  for value in values[1:]:
+    new_node = Node(value)
+    current.next = new_node
+    current = new_node
+  return head
+
