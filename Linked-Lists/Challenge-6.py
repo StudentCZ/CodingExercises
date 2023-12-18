0236c864dda8770d5d53c6d8da2bfdcd56384266
Chao Zhou <Czhou0k@gmail.com>
2023-12-18 13:06:57 -0500

    added a logger for link list

--- Linked-Lists/Challenge-6.py
+++ Linked-Lists/Challenge-6.py
@@ -2,3 +2,10 @@ class Node:
   def __init__(self,val) -> None:
     self.val = val
     self.next = None
+
+def print_linked_list(head):
+  current = head
+  while current is not None:
+    print(current.val)
+    current = current.next
+
