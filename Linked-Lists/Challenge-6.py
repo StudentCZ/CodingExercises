d2c3f40bd394be6ba34274fa0cc1a889bf1c96d0
Chao Zhou <Czhou0k@gmail.com>
2023-12-18 13:31:40 -0500

    added a reverse helper function and solution to challenge 6 python

--- Linked-Lists/Challenge-6.py
+++ Linked-Lists/Challenge-6.py
@@ -19,3 +19,39 @@ def create_linked_list(values):
     current = new_node
   return head
 
+values = ["A",'B','B','A']
+head = create_linked_list(values)
+
+def reverse(head):
+  current = head
+  prev = None
+
+  while (current is not None):
+    next = current.next
+    current.next = prev
+    prev = current
+    current = next
+  return prev
+
+
+def palindrome_linked_list(head):
+  current = head
+  slow = head
+  fast = head
+
+  while fast is not None and fast.next is not None:
+    slow = slow.next
+    fast = fast.next.next
+
+  slow_reverse = reverse(slow.next)
+
+  while slow_reverse is not None:
+    if current.val != slow_reverse.val:
+      return False
+    slow_reverse = slow_reverse.next
+    current = current.next
+  return True
+
+result = palindrome_linked_list(head)
+print(result)
+
