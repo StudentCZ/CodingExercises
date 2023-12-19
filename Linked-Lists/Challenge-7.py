0646e0c6ea1ecdccd7a3c0ead6925f042626b607
Chao Zhou <Czhou0k@gmail.com>
2023-12-19 18:11:17 -0500

    added 2 example linked list to test intersection

--- Linked-Lists/Challenge-7.py
+++ Linked-Lists/Challenge-7.py
@@ -17,6 +17,28 @@ def get_length(head):
     current = current.next
   return length
 
+head1 = Node('A')
+node2 = Node('B')
+node3 = Node('C')
+node4 = Node('D')
+node5 = Node('E')
+
+head1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
+
+head2 = Node('S')
+node_a2 = node4
+head2.next = node_a2
+
 def intersect_linked_list(head1,head2):
   pointer_one = head1
   pointer_two = head2
+
+  head_one_length = get_length(pointer_one)
+  head_two_length = get_length(pointer_two)
+  print(head_one_length)
+  print(head_two_length)
+
+
