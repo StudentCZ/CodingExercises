a73406ad367f497585853fafd00d67f3e5cef9ec
Chao Zhou <Czhou0k@gmail.com>
2023-12-19 18:05:05 -0500

    added a helper function to get the length in case linked lists are different length

--- Linked-Lists/Challenge-7.py
+++ Linked-Lists/Challenge-7.py
@@ -8,3 +8,15 @@ def print_linked_list(head):
   while current is not None:
     print(current.val)
     current = current.next
+
+def get_length(head):
+  length = 0
+  current = head
+  while current is not None:
+    length += 1
+    current = current.next
+  return length
+
+def intersect_linked_list(head1,head2):
+  pointer_one = head1
+  pointer_two = head2
