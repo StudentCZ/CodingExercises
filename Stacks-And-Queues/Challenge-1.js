ce7f02e8850561b3cf31863eedb870577b826d8b
Chao Zhou <Czhou0k@gmail.com>
2023-12-28 10:41:19 -0500

    added logic to separate array into 3 parts

---
+++ Stacks-And-Queues/Challenge-1.js
@@ -0,0 +1,7 @@
+const tripleStack = (array) => {
+  const length = Math.floor(array.length / 3);
+
+  const firstStack = array.slice(0, length);
+  const secondStack = array.slice(length, 2 * length);
+  const thirdStack = array.slice(2 * length);
+};
