8b6602edb7a242da3474310af597536982b43b68
Chao Zhou <Czhou0k@gmail.com>
2024-01-01 13:02:14 -0500

    revised pop function

--- Stacks-And-Queues/Challenge-1.js
+++ Stacks-And-Queues/Challenge-1.js
@@ -11,8 +11,13 @@ const tripleStack = (array) => {
       console.log(`Stack ${stackNumber + 1} is full. Cannot push ${value}.`);
     }
   };
-};
-
-const pop = (stackNumber) => {
-  stackNumber.pop();
+  const pop = (stackNumber) => {
+    if (top[stackNumber] > stackNumber * length) {
+      const value = array[top[stackNumber]];
+      top[stackNumber]--;
+      return value;
+    } else {
+      console.log(`Stack ${stackNumber + 1} is empty. Cannot pop.`);
+    }
+  };
 };
