caa69c5cd4ef9626478ac04067a6a23fe0aa8784
Chao Zhou <Czhou0k@gmail.com>
2024-01-01 12:58:22 -0500

    revised the push function

--- Stacks-And-Queues/Challenge-1.js
+++ Stacks-And-Queues/Challenge-1.js
@@ -2,10 +2,15 @@ const tripleStack = (array) => {
   const length = Math.floor(array.length / 3);
 
   const top = [0, length, 2 * length];
-};
 
-const push = (stackNumber, value) => {
-  stackNumber.push(value);
+  const push = (stackNumber, value) => {
+    if (top[stackNumber] < (stackNumber + 1) * length) {
+      array[top[stackNumber]] = value;
+      top[stackNumber]++;
+    } else {
+      console.log(`Stack ${stackNumber + 1} is full. Cannot push ${value}.`);
+    }
+  };
 };
 
 const pop = (stackNumber) => {
