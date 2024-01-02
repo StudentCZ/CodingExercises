ba544738b09755b6bed6afc6a3e8589b4f9ff426
Chao Zhou <Czhou0k@gmail.com>
2024-01-02 16:43:23 -0500

    added test and finished turning function component into class component

--- Stacks-And-Queues/Challenge-1.js
+++ Stacks-And-Queues/Challenge-1.js
@@ -1,40 +1,40 @@
-const tripleStack = (array) => {
-  const length = Math.floor(array.length / 3);
+// const tripleStack = (array) => {
+//   const length = Math.floor(array.length / 3);
 
-  const top = [0, length, 2 * length];
+//   const top = [0, length, 2 * length];
 
-  const push = (stackNumber, value) => {
-    if (top[stackNumber] < (stackNumber + 1) * length) {
-      array[top[stackNumber]] = value;
-      top[stackNumber]++;
-    } else {
-      console.log(`Stack ${stackNumber + 1} is full. Cannot push ${value}.`);
-    }
-  };
-  const pop = (stackNumber) => {
-    if (top[stackNumber] > stackNumber * length) {
-      const value = array[top[stackNumber]];
-      top[stackNumber]--;
-      return value;
-    } else {
-      console.log(`Stack ${stackNumber + 1} is empty. Cannot pop.`);
-    }
-  };
+//   const push = (stackNumber, value) => {
+//     if (top[stackNumber] < (stackNumber + 1) * length) {
+//       array[top[stackNumber]] = value;
+//       top[stackNumber]++;
+//     } else {
+//       console.log(`Stack ${stackNumber + 1} is full. Cannot push ${value}.`);
+//     }
+//   };
+//   const pop = (stackNumber) => {
+//     if (top[stackNumber] > stackNumber * length) {
+//       const value = array[top[stackNumber]];
+//       top[stackNumber]--;
+//       return value;
+//     } else {
+//       console.log(`Stack ${stackNumber + 1} is empty. Cannot pop.`);
+//     }
+//   };
 
-  const inspect = () => ({ array, top });
-  return { push, pop, inspect };
-};
+//   const inspect = () => ({ array, top });
+//   return { push, pop, inspect };
+// };
 
-const stacks = tripleStack(new Array(9));
+// const stacks = tripleStack(new Array(9));
 
-stacks.push(0, 1);
-stacks.push(0, 4);
-stacks.push(1, 2);
-stacks.push(1, 5);
-stacks.push(2, 3);
-stacks.push(2, 6);
+// stacks.push(0, 1);
+// stacks.push(0, 4);
+// stacks.push(1, 2);
+// stacks.push(1, 5);
+// stacks.push(2, 3);
+// stacks.push(2, 6);
 
-console.log(stacks.inspect());
+// console.log(stacks.inspect());
 
 class TripleStack {
   constructor(array) {
@@ -59,4 +59,15 @@ class TripleStack {
       console.log(`Stack ${stackNumber + 1} is empty. Cannot pop.`);
     }
   }
+  inspect() {
+    return { array: this.array, top: this.top };
+  }
 }
+
+const stacks = new TripleStack(new Array(9));
+
+stacks.push(0, 1);
+stacks.push(1, 2);
+stacks.push(2, 3);
+
+console.log(stacks.inspect());
