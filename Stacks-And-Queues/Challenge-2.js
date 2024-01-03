c4a4739e668a3b052b8da5e9f5b2a06a644d5385
Chao Zhou <Czhou0k@gmail.com>
2024-01-03 15:15:39 -0500

    added a push function into create min stack

--- Stacks-And-Queues/Challenge-2.js
+++ Stacks-And-Queues/Challenge-2.js
@@ -1,4 +1,12 @@
 function createMinStack() {
   const stack = [];
   const minStack = [];
+
+  function push(value) {
+    stack.push(value);
+
+    if (minStack.length === 0 || value <= minStack[minStack.length - 1]) {
+      minStack.push(value);
+    }
+  }
 }
