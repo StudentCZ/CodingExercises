72bc114bc61114eb70f4754ced6aa01ddf3d6cf0
Chao Zhou <Czhou0k@gmail.com>
2024-01-05 16:02:08 -0500

    created a push function

--- Stacks-And-Queues/Challenge-3.js
+++ Stacks-And-Queues/Challenge-3.js
@@ -1,3 +1,13 @@
 function createSetStack(capacity) {
   const stacks = [[]];
+
+  function push(value) {
+    const currentStack = stacks[stacks.length - 1];
+
+    if (currentStack.length < capacity) {
+      currentStack.push(value);
+    } else {
+      stacks.push([value]);
+    }
+  }
 }
