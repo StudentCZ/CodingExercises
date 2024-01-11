89494a8639c889f27860ef13cb9f0950d929b0c8
Chao Zhou <Czhou0k@gmail.com>
2024-01-11 16:17:54 -0500

    added test to check solution and fixed typo with conditional statement check

--- Stacks-And-Queues/Challenge-4.js
+++ Stacks-And-Queues/Challenge-4.js
@@ -1,30 +1,30 @@
-function myQueue() {
-  const stackPush = [];
-  const stackPop = [];
-  function push(element) {
-    stackPush.push(element);
-  }
-  function pop() {
-    if (stackPop.length === 0) {
-      while (stackPush.length > 0) {
-        stackPop.push(stackPush.pop());
-      }
-    }
-    if (stackPop.length === 0) {
-      return undefined;
-    }
-    return stackPop.pop();
-  }
-  return { push, pop };
-}
+// function myQueue() {
+//   const stackPush = [];
+//   const stackPop = [];
+//   function push(element) {
+//     stackPush.push(element);
+//   }
+//   function pop() {
+//     if (stackPop.length === 0) {
+//       while (stackPush.length > 0) {
+//         stackPop.push(stackPush.pop());
+//       }
+//     }
+//     if (stackPop.length === 0) {
+//       return undefined;
+//     }
+//     return stackPop.pop();
+//   }
+//   return { push, pop };
+// }
 
-const queue = myQueue();
-queue.push(1);
-queue.push(2);
-queue.push(3);
-queue.push(4);
-console.log(queue.pop());
-console.log(queue.pop());
+// const queue = myQueue();
+// queue.push(1);
+// queue.push(2);
+// queue.push(3);
+// queue.push(4);
+// console.log(queue.pop());
+// console.log(queue.pop());
 
 class MyQueue {
   constructor() {
@@ -40,9 +40,16 @@ class MyQueue {
         this.stackPop.push(this.stackPush.pop());
       }
     }
-    if (this.stackPush.length === 0) {
+    if (this.stackPop.length === 0) {
       return undefined;
     }
     return this.stackPop.pop();
   }
 }
+
+const queue2 = new MyQueue();
+queue2.push(5);
+queue2.push(4);
+queue2.push(1);
+queue2.push(2);
+console.log(queue2.pop());
