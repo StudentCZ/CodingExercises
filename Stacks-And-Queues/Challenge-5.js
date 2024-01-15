e1e58d7ad2762844fbd3f020ae01395ea9057970
Chao Zhou <Czhou0k@gmail.com>
2024-01-14 19:13:57 -0500

    added test to test solution and got a working sort stack function

--- Stacks-And-Queues/Challenge-5.js
+++ Stacks-And-Queues/Challenge-5.js
@@ -1,1 +1,19 @@
-function sortStack(mainStack) {}
+function sortStack(mainStack) {
+  const tempStack = [];
+
+  while (mainStack.length > 0) {
+    const temp = mainStack.pop();
+    while (tempStack.length > 0 && tempStack[tempStack.length - 1] > temp) {
+      mainStack.push(tempStack.pop());
+    }
+    tempStack.push(temp);
+  }
+
+  while (tempStack.length > 0) {
+    mainStack.push(tempStack.pop());
+  }
+}
+
+const unsortedStack = [4, 3, 2, 5, 1];
+sortStack(unsortedStack);
+console.log(unsortedStack);
