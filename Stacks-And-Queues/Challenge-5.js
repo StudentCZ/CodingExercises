081eaea16b674e4e4a16775636f24b9d758b78d9
Chao Zhou <Czhou0k@gmail.com>
2024-01-16 17:00:17 -0500

    fixed logic of solution and added test to test solution

--- Stacks-And-Queues/Challenge-5.js
+++ Stacks-And-Queues/Challenge-5.js
@@ -1,22 +1,22 @@
-function sortStack(mainStack) {
-  const tempStack = [];
+// function sortStack(mainStack) {
+//   const tempStack = [];
 
-  while (mainStack.length > 0) {
-    const temp = mainStack.pop();
-    while (tempStack.length > 0 && tempStack[tempStack.length - 1] > temp) {
-      mainStack.push(tempStack.pop());
-    }
-    tempStack.push(temp);
-  }
+//   while (mainStack.length > 0) {
+//     const temp = mainStack.pop();
+//     while (tempStack.length > 0 && tempStack[tempStack.length - 1] > temp) {
+//       mainStack.push(tempStack.pop());
+//     }
+//     tempStack.push(temp);
+//   }
 
-  while (tempStack.length > 0) {
-    mainStack.push(tempStack.pop());
-  }
-}
+//   while (tempStack.length > 0) {
+//     mainStack.push(tempStack.pop());
+//   }
+// }
 
-const unsortedStack = [4, 3, 2, 5, 1];
-sortStack(unsortedStack);
-console.log(unsortedStack);
+// const unsortedStack = [4, 3, 2, 5, 1];
+// sortStack(unsortedStack);
+// console.log(unsortedStack);
 
 class SortStack {
   constructor() {
@@ -29,13 +29,19 @@ class SortStack {
 
       while (
         this.tempStack.length > 0 &&
-        this.tempStack[this.tempStack.length - 1] > temp
+        this.tempStack[this.tempStack.length - 1] < temp
       ) {
         this.mainStack.push(this.tempStack.pop());
       }
+      this.tempStack.push(temp);
     }
     while (this.tempStack.length > 0) {
       this.mainStack.push(this.tempStack.pop());
     }
   }
 }
+
+const sortStack = new SortStack();
+sortStack.mainStack.push(3, 4, 5, 1, 2);
+sortStack.sort();
+console.log(sortStack.mainStack);
