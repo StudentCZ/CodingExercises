03e33fb2f4e4862cef54d0913a33ce265c52ec92
Chao Zhou <Czhou0k@gmail.com>
2024-01-19 13:25:57 -0500

    finished creating the enqueue method to sort out dog and cat entry

--- Stacks-And-Queues/Challenge-6.js
+++ Stacks-And-Queues/Challenge-6.js
@@ -4,6 +4,18 @@ class AnimalShelter {
     this.catQueue = [];
     this.order = 0;
   }
+  enqueue(animal, type) {
+    const entry = { animal, type, order: this.order++ };
+    if (type === 'dog') {
+      this.dogQueue.push(entry);
+    } else if (type === 'cat') {
+      this.catQueue.push(entry);
+    } else {
+      throw new Error(
+        'Invalid animal type. Supported types are "dog" and "cat".'
+      );
+    }
+  }
 }
 
 const createAnimalShelter = () => {
