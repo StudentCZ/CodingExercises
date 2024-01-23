e8a4ae2355384544778f4ea846f0a13e7d00ecd5
Chao Zhou <Czhou0k@gmail.com>
2024-01-23 13:13:40 -0500

    added push method to class

--- Stacks-And-Queues/Challenge-1.py
+++ Stacks-And-Queues/Challenge-1.py
@@ -3,3 +3,11 @@ class TripleStack:
     self.stack_size = stack_size
     self.array = [None] * (stack_size * 3)
     self.top = [0,0,0]
+
+  def push(self, stack_number, value):
+    if self.top[stack_number] < self.stack_size:
+      index = stack_number * self.stack_size + self.top[stack_number]
+      self.array[index] = value
+      self.top[stack_number] += 1
+    else:
+      print(f"Stack {stack_number} is full. Cannot push {value}.")
