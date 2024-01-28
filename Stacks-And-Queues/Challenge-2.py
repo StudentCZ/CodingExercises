8b0587b1547ce928515b401844a8b00103beb9a9
Chao Zhou <Czhou0k@gmail.com>
2024-01-27 22:06:03 -0500

    added push method to class

--- Stacks-And-Queues/Challenge-2.py
+++ Stacks-And-Queues/Challenge-2.py
@@ -2,3 +2,7 @@ class MinStack:
   def __init__(self) -> None:
     self.stack = []
     self.min_stack = []
+  def push(self, value):
+    self.stack.append(value)
+    if len(self.min_stack) == 0 or value <= self.min_stack[-1]:
+      self.min_stack.append(value)
