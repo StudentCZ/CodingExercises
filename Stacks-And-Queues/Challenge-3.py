f36c30f03d3f76c20ba252a07fcc18ea4a54e33f
Chao Zhou <Czhou0k@gmail.com>
2024-01-30 19:09:25 -0500

    added push function to class

--- Stacks-And-Queues/Challenge-3.py
+++ Stacks-And-Queues/Challenge-3.py
@@ -3,3 +3,9 @@ class SetOfStacks:
     self.capacity = capacity
     self.stack = []
     self.current_stack = []
+  def push(self,item):
+    if len(self.current_stack) == self.capacity:
+      self.stack.append(self.current_stack)
+      self.current_stack = [item]
+    else:
+      self.current_stack.append(item)
