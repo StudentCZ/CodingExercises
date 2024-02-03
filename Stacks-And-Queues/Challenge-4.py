fd021ae965a2a78376c8645726d77a80d146f5aa
Chao Zhou <Czhou0k@gmail.com>
2024-02-02 21:10:29 -0500

    added dequeue function to class

--- Stacks-And-Queues/Challenge-4.py
+++ Stacks-And-Queues/Challenge-4.py
@@ -4,4 +4,11 @@ class MyQueue:
     self.stack_two = []
   def enqueue(self,value):
     self.stack_one.append(value)
-
+  def dequeue(self):
+    if not self.stack_two:
+      while self.stack_one:
+        self.stack_two.append(self.stack_one.pop())
+    if self.stack_two:
+      return self.stack_two.pop()
+    else:
+      return None
