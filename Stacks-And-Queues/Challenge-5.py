016c7d6c507d38730d1f1bc9509191d844c49fab
Chao Zhou <Czhou0k@gmail.com>
2024-02-04 20:36:25 -0500

    revise while loop to sort stack in ascending order

--- Stacks-And-Queues/Challenge-5.py
+++ Stacks-And-Queues/Challenge-5.py
@@ -4,7 +4,7 @@ def sort_stack(stack):
   while stack:
     temp = stack.pop()
 
-    while temp_stack and temp_stack[-1] > temp:
+    while temp_stack and temp_stack[-1] < temp:
       stack.append(temp_stack.pop())
 
     temp_stack.append(temp)
