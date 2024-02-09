590f413395247241c76144429ac92e1b4ab69eec
Chao Zhou <Czhou0k@gmail.com>
2024-02-08 20:23:31 -0500

    change push function to ascending order

--- Stacks-And-Queues/Challenge-5.py
+++ Stacks-And-Queues/Challenge-5.py
@@ -1,20 +1,20 @@
-def sort_stack(stack):
-  temp_stack = []
+# def sort_stack(stack):
+#   temp_stack = []
 
-  while stack:
-    temp = stack.pop()
+#   while stack:
+#     temp = stack.pop()
 
-    while temp_stack and temp_stack[-1] < temp:
-      stack.append(temp_stack.pop())
+#     while temp_stack and temp_stack[-1] < temp:
+#       stack.append(temp_stack.pop())
 
-    temp_stack.append(temp)
+#     temp_stack.append(temp)
 
-  while temp_stack:
-    stack.append(temp_stack.pop())
+#   while temp_stack:
+#     stack.append(temp_stack.pop())
 
-stack = [3,4,1,5,2]
-sort_stack(stack)
-print(stack)
+# stack = [3,4,1,5,2]
+# sort_stack(stack)
+# print(stack)
 
 class SortedStack:
   def __init__(self):
@@ -23,7 +23,7 @@ class SortedStack:
 
   def push(self,value):
 
-    while self.stack and self.stack[-1] < value:
+    while self.stack and self.stack[-1] > value:
       self.temp_stack.append(self.stack.pop())
 
     self.stack.append(value)
